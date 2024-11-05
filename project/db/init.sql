6ac6c57820260f490fb6507b941e20d875b34783
ppark123 <pearlpark8798@gmail.com>
2024-11-05 14:59:24 -0800

    adding sql init file fixed

--- project/db/init.sql
+++ project/db/init.sql
@@ -1,3 +1,10 @@
+DROP TABLE Restaurant;
+DROP TABLE Branch;
+DROP TABLE FOOD_INFORMATION;
+DROP TABLE Food;
+DROP TABLE Coupon;
+DROP TABLE Sells;
+
 CREATE TABLE Restaurant (
     name VARCHAR(1000) PRIMARY KEY,
     type VARCHAR(1000)
@@ -34,13 +41,16 @@ CREATE TABLE Coupon (
     number_of_uses INTEGER,
     FOREIGN KEY(branch_id) REFERENCES Branch ON DELETE CASCADE
 );
+
 CREATE TABLE Sells (
-    food_name CHAR(8),
+    food_name VARCHAR(1000),
+    f_size VARCHAR(1000),
     branch_id CHAR(5),
     PRIMARY KEY (food_name, branch_id),
-    FOREIGN KEY (food_name) REFERENCES Food ON DELETE CASCADE,
-    FOREIGN KEY (branch_id) REFERENCES Branch ON DELETE CASCADE
+    FOREIGN KEY (food_name, f_size) REFERENCES Food(food_name, f_size) ON DELETE CASCADE,
+    FOREIGN KEY (branch_id) REFERENCES Branch(branch_id)  ON DELETE CASCADE
 );
+
 --Insert statements to populate the db
 INSERT INTO Restaurant VALUES('Starbucks', 'cafe');
 INSERT INTO Restaurant VALUES('Triple_Os', 'restaurant');
@@ -67,8 +77,8 @@ INSERT INTO Coupon VALUES('B152R99G', 'T0001', 0.25, 2);
 INSERT INTO Coupon VALUES('K0E5G001', 'H0001', 0.02, 1);
 INSERT INTO Coupon VALUES('B0F13D01', 'H0001', 0.05, 0);
 INSERT INTO Coupon VALUES('P0F33N20', 'P0001', 0.10, 0);
-INSERT INTO Sells VALUES('americano', 'S0002');
-INSERT INTO Sells VALUES('cheese_burger', 'T0001');
-INSERT INTO Sells VALUES('ceasar_salad', 'H0001');
-INSERT INTO Sells VALUES('roasted_chicken_sub', 'S0001');
-INSERT INTO Sells VALUES('main_bowl', 'P0001');
+INSERT INTO Sells VALUES('americano', 'small','S0002');
+INSERT INTO Sells VALUES('cheese_burger','large', 'T0001');
+INSERT INTO Sells VALUES('ceasar_salad', 'regular','H0001');
+INSERT INTO Sells VALUES('roasted_chicken_sub', 'foot_long','S0001');
+INSERT INTO Sells VALUES('main_bowl', 'regular','P0001');
