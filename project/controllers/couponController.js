2b2e312228967353e8de293c692847739a63da10
ppark123 <pearlpark8798@gmail.com>
2024-11-06 11:47:20 -0800

    adding controller for coupons

---
+++ project/controllers/couponController.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const appService = require('../appService');
+const router = express.Router();
+
+// ----------------------------------------------------------
+// API endpoints for coupon:
+
+//fetch the coupons
+router.get('/coupons', async (req, res) => {
+    const couponTable = await appService.fetchCoupon();
+    res.json({data: couponTable});
+});
+
+//update the number of uses of the select coupon
+router.post('/coupons/:cid', async (req, res) => {
+    const coupon_id = req.params.cid;
+    const coupon = await appService.updateNumberUses(coupon_id);
+    res.json({data: coupon});
+});
+
+// delete coupons with number of uses == 0
+router.delete('/coupons/:cid', async (req, res) => {
+    const coupon_id = req.params.cid;
+    await appService.deleteCoupon(coupon_id);
+});
+
+// retrieve coupons with only one number of uses left
+router.get('/coupons', async(req,res) => {
+    const oneUseCoupons = await appService.retrieveOneUseCoupons();
+    res.json({data: oneUseCoupons})
+});
+
+module.exports = router;
