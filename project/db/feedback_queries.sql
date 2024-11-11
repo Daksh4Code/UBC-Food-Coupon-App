bbf218dae0a7719a0dc67b48e5abf17674acf145
daksh <dmathu01@students.cs.ubc.ca>
2024-11-10 22:12:50 -0800

    Added SQL queries for user and feedback

---
+++ project/db/feedback_queries.sql
@@ -0,0 +1,25 @@
+-- Insert feedback
+INSERT INTO Feedback_Rating (account_id, sid, order_date, branch_id, rating) VALUES (:accountId, :sid, :order_date, :branchId, :rating);
+
+-- Update feedback
+UPDATE Feedback_Rating
+SET rating = :newRating
+WHERE account_id = :accountId AND sid = :sid AND order_date = :order_date AND branch_id = :branchId;
+
+-- View feedback (for a specific user)
+SELECT *
+FROM Feedback_Rating
+WHERE account_id = :accountId;
+
+-- Nested aggregation with GROUP BY (to find the best average rating)
+SELECT branch_id
+FROM Feedback_Rating
+GROUP BY branch_id
+HAVING AVG(rating) >= (
+    SELECT MAX(avg_rating)
+    FROM (
+             SELECT AVG(rating) AS avg_rating
+             FROM Feedback_Rating
+             GROUP BY branch_id
+         )
+);
---
+++ project/db/user_queries.sql
@@ -0,0 +1,13 @@
+-- User Creation (allowing multiple accounts with the same CWL)
+-- The primary key is on account_id, so multiple accounts with the same CWL are allowed.
+INSERT INTO Account (account_id, year, major, password, sid, cwl) VALUES (:account_id, :year, :major, :password, :sid, :cwl);
+
+-- Edit Password
+UPDATE Account
+SET password = :newPassword
+WHERE account_id = :accountId;
+
+-- Sign in with the account made (Projection Query)
+SELECT account_id, year, major, sid, cwl
+FROM Account
+WHERE cwl = :cwl AND password = :password;
