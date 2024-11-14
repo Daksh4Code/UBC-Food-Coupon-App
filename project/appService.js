7d0498198395be21e77a9ba30c483721dbcfcce9
ppark123 <pearlpark8798@gmail.com>
2024-11-13 19:33:00 -0800

    fixing/adding files

--- project/appService.js
+++ project/appService.js
@@ -1,6 +1,5 @@
 const oracledb = require('oracledb');
 const loadEnvFile = require('./utils/envUtil');
-const fs = require('fs');
 
 const envVariables = loadEnvFile('./.env');
 
@@ -65,6 +64,10 @@ async function withOracleDB(action) {
     }
 }
 
+
+// ----------------------------------------------------------
+// Core functions for database operations
+// Modify these functions, especially the SQL queries, based on your project's requirements and design.
 async function testOracleConnection() {
     return await withOracleDB(async (connection) => {
         return true;
@@ -73,6 +76,77 @@ async function testOracleConnection() {
     });
 }
 
+async function fetchDemotableFromDb() {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute('SELECT * FROM DEMOTABLE');
+        return result.rows;
+    }).catch(() => {
+        return [];
+    });
+}
+
+async function initiateDemotable() {
+    return await withOracleDB(async (connection) => {
+        try {
+            await connection.execute(`DROP TABLE DEMOTABLE`);
+        } catch(err) {
+            console.log('Table might not exist, proceeding to create...');
+        }
+
+        const result = await connection.execute(`
+            CREATE TABLE DEMOTABLE (
+                id NUMBER PRIMARY KEY,
+                name VARCHAR2(20)
+            )
+        `);
+        return true;
+    }).catch(() => {
+        return false;
+    });
+}
+
+async function insertDemotable(id, name) {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute(
+            `INSERT INTO DEMOTABLE (id, name) VALUES (:id, :name)`,
+            [id, name],
+            { autoCommit: true }
+        );
+
+        return result.rowsAffected && result.rowsAffected > 0;
+    }).catch(() => {
+        return false;
+    });
+}
+
+async function updateNameDemotable(oldName, newName) {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute(
+            `UPDATE DEMOTABLE SET name=:newName where name=:oldName`,
+            [newName, oldName],
+            { autoCommit: true }
+        );
+
+        return result.rowsAffected && result.rowsAffected > 0;
+    }).catch(() => {
+        return false;
+    });
+}
+
+async function countDemotable() {
+    return await withOracleDB(async (connection) => {
+        const result = await connection.execute('SELECT Count(*) FROM DEMOTABLE');
+        return result.rows[0][0];
+    }).catch(() => {
+        return -1;
+    });
+}
+
 module.exports = {
-    testOracleConnection
+    testOracleConnection,
+    fetchDemotableFromDb,
+    initiateDemotable,
+    insertDemotable,
+    updateNameDemotable,
+    countDemotable
 };
